87ecfca1f59851026790dbb19edf6ebac7eb00b6
hugo-onnx <hugonzalezhuerta@gmail.com>
2026-03-05 23:37:32 +0100

    refactor: update feature assembly to use idle_drivers

--- services/inference-api/eta/features.py
+++ services/inference-api/eta/features.py
@@ -23,7 +23,7 @@ def assemble_pickup_features(
         "supply_demand_ratio": features_5m["supply_demand_ratio"],
         "surge_pressure": features_5m["surge_pressure"],
         "deadhead_km_avg": features_5m["deadhead_km_avg"],
-        "available_drivers": features_5m["available_drivers"],
+        "idle_drivers": features_5m["idle_drivers"],
         "ride_requests": features_5m["ride_requests"],
     }
 
